5c56c196cbd117485bafae402d0be9bf281a043f
Derek Chung Lap Cheung <CHEUNGDEREK@HOTMAIL.COM>
2020-11-30 18:00:42 -0500

    last commit before PUSHING to remote GITHUB. After dinner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://medium.com/analytics-vidhya/building-an-intrinsic-value-calculator-with-python-7986833962cd
 # https://medium.com/swlh/automating-your-stock-portfolio-research-with-python-for-beginners-912dc02bf1c2
 # Derek - 30 Nov 2020 after learning the youtUbe GIT toturial
+# after dinner
 #
 import requests
 import pandas as pd
